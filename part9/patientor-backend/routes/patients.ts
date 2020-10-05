01cd97771bf1c23737a32975800f9a527e9842f5
gouxi <gouxivae@gmail.com>
2020-10-06 04:39:43 +0800

    9.12 is done.

--- part9/patientor-backend/routes/patients.ts
+++ part9/patientor-backend/routes/patients.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { patientsData } from "../data/patients";
 import { Patient } from "../types";
+import {v4 as uuid} from "uuid";
 
 const patientsRouter = express.Router();
+let data = patientsData;
 
 patientsRouter.get("/", (_req, res) =>{
   const resData = patientsData.map(
@@ -15,5 +17,11 @@ patientsRouter.get("/", (_req, res) =>{
   res.json(resData);
 });
 
+patientsRouter.post("/", (req, res) => {
+  const body = req.body;
+  body.id = uuid();
+  data = data.concat(body as Patient);
+  res.json(body);
+});
 
 export default patientsRouter;
